93e643074ba041d92ecf70010517c237cabfc58a
Dirk Winkhaus <dirk.winkhaus@check24.de>
2018-11-12 18:27:16 +0100

    new structure

--- bin/compile.bat
+++ bin/compile.bat
@@ -9,6 +9,6 @@ pause
 goto end_of_file
 :create_disc
 del ..\preOS.iso
-ultraiso -volume myVolume -sysid mySysId -preparer prepdirk -publisher pubdirk -joliet -bootfile ..\bootLoader.bin -output ..\preOS.iso -file .\cdcontent\file-a.txt -file .\cdcontent\file-b.txt -file .\cdcontent\filec.txt -file .\cdcontent\terminal.do -file .\cdcontent\reboot.do -file .\cdcontent\play.do -directory .\cdcontent\directory
+ultraiso -volume myVolume -sysid mySysId -preparer widi -publisher widi -joliet -bootfile ..\bootloader.bin -output ..\preOS.iso -file ..\cdcontent\file-a.txt -file ..\cdcontent\file-b.txt -file ..\cdcontent\filec.txt -file ..\cdcontent\terminal.do -file ..\cdcontent\reboot.do -file ..\cdcontent\play.do -directory ..\cdcontent\directory
 :end_of_file
 cd ../bin
