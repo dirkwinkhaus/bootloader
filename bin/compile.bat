8909b9d04fe439b82c807ea58dec49934057fea2
Dirk Winkhaus <dirk.winkhaus@check24.de>
2018-11-13 07:59:36 +0100

    fix broken compile

--- bin/compile.bat
+++ bin/compile.bat
@@ -1,8 +1,8 @@
-#@echo off
-cd ..\src\core
+@echo off
+cd ..\src\kernel
 
-@echo create binary core file
-nasm core.asm -f bin -o ..\..\rc\core.bin 
+@echo create binary kernel
+nasm kernel.asm -f bin -o ..\..\rc\kernel.bin 
 if errorlevel 1 goto display_error
 goto create_disc
 :display_error
@@ -10,6 +10,6 @@ pause
 goto end_of_file
 :create_disc
 del ..\..\rc\preOS.iso
-ultraiso -volume myVolume -sysid mySysId -preparer widi -publisher widi -joliet -bootfile ..\..\rc\core.bin -output ..\..\rc\preOS.iso -file ..\..\disc_data\file-a.txt -file ..\..\disc_data\file-b.txt -file ..\..\disc_data\filec.txt -file ..\..\disc_data\terminal.do -file ..\..\disc_data\reboot.do -directory ..\..\disc_data\directory
+ultraiso -volume myVolume -sysid mySysId -preparer widi -publisher widi -joliet -bootfile ..\..\rc\kernel.bin -output ..\..\rc\preOS.iso -file ..\..\disc_data\text_file.txt -file ..\..\disc_data\terminal.do -file ..\..\disc_data\reboot.do -directory ..\..\disc_data\directory
 :end_of_file
 cd ..\..\bin
